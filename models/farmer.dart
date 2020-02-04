24bc6ca7e3a32ff2625c58c2a8a73cc82340e135
shunjid <shunjid1862@gmail.com>
2020-02-05 01:07:23 +0600

    added required models, constructed api

---
+++ models/farmer.dart
@@ -0,0 +1,11 @@
+import 'foodCrops.dart';
+
+class farmer {
+  int farmerId;
+  String farmerLocation;
+  int farmerAge;
+  double farmerMonthlyIncome;
+  foodCrops fc;
+
+  farmer(this.farmerId, this.farmerLocation, this.farmerAge, this.farmerMonthlyIncome, this.fc);
+}
---
+++ models/foodCrops.dart
@@ -0,0 +1,7 @@
+class foodCrops {
+  int id;
+  String cropType;
+  String cropName;
+
+  foodCrops(this.id, this.cropType, this.cropName);
+}
